d6b2c25301f19195dc4549b65d85342d491ce7ce
yukinotech <yukinotech@gmail.com>
2019-04-11 11:24:46 +0800

    0.1

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"github.com/yukinotech/FirstGoPackage"
 )
 
 func main(){
 	fmt.Println("Hello, World!")
+	FirstGoPackage.Say('good bye')
+}
+
+func Say(words string){ 
+	fmt.Println(words)
 }
